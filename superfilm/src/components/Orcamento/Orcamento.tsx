854c7afd5a1e1412625f464b19d33bb0b67fffe9
Cainã Milech <cainamilech15@hotmail.com>
2026-07-29 22:57:38 -0300

    feat: atualiza titulo

--- superfilm/src/components/Orcamento/Orcamento.tsx
+++ superfilm/src/components/Orcamento/Orcamento.tsx
@@ -51,7 +51,7 @@ const Orcamento = () => {
         <div className={orcamento["container__mapa"]}>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5000.833117863634!2d-48.49703861585817!3d-27.589715705584805!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95273f381a2ccf7d%3A0x7d0f5a8ad8d2d1a!2sSuperfilm%20-%20Pel%C3%ADculas%20Residenciais!5e0!3m2!1spt-BR!2sbr!4v1784563957710!5m2!1spt-BR!2sbr"
-
+            title="mapa superfilm"
             allowFullScreen
             loading="lazy"
             referrerPolicy="strict-origin-when-cross-origin"
